78b14c5cab84a995e00980daeaaf35f81afa9cc6
heeeione <heewon410@naver.com>
2024-10-18 15:18:12 +0900

    init: Init project

---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "../layout/DefaultLayout";
+import Home from "../pages/Home";
+
+const routes = [
+    {
+        path: '/',
+        element: <Home />,
+    }
+];
+
+const router = createBrowserRouter([
+    {
+        element: <DefaultLayout />,
+        children: routes,
+    },
+]);
+
+export default router;
