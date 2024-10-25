25ab07c0e07c304cffa504b3876e25ff3663b6d2
gwagjiug <kwjo0228@naver.com>
2024-10-25 13:40:24 +0900

    notice header border-bottom 추가

--- src/style/notice/notice.js
+++ src/style/notice/notice.js
@@ -6,6 +6,7 @@ export const NoticeHeader = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 1rem;
+  border-bottom: 1px solid #ccc;
 
   .header__icon {
     transition: color 0.3s ease;
