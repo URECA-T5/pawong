183d5edaea2872dd77734d73a8959a60d00991a4
iam-eungin <1618232@donga.ac.kr>
2024-10-25 22:21:03 +0900

    fix: Navbar position 변경

--- src/style/main/navbar.js
+++ src/style/main/navbar.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Navbar = styled.section`
   width: 100%;
   height: 53px;
-  position: sticky;
+  position: fixed;
   align-items: center;
   bottom: 0;
   left: 0;
