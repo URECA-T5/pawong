cf15dbe675da334b3d8a8e7482fd81b3b46e3e52
heeeione <heewon410@naver.com>
2024-10-25 17:07:43 +0900

    refactor: Generalize layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import GlobalStyle, { MainContainer } from '../style/global/global';
 import { LoginHeader, LoginBody } from '../style/login/login';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
@@ -8,34 +7,31 @@ import GoogleLoginButton from '../components/auth/GoogleLoginButton';
 const Login = () => {
   return (
     <>
-      <GlobalStyle />
-      <MainContainer>
-        <LoginHeader>
-          <FontAwesomeIcon icon={faHouse} />
-        </LoginHeader>
-        <LoginBody>
-          <h4 className="login__h4">반려동물 1등 플랫폼</h4>
-          <p className="login__p">모든 반려동물의 행복을 바라며</p>
-          <h1 className="login__h1 extraBold">포옹</h1>
-          <div className="img__section">
-            <img
-              src="/asset/login/Login_Img.svg"
-              alt="로그인 메인이미지"
-              className="login__img"
-            />
-          </div>
+      <LoginHeader>
+        <FontAwesomeIcon icon={faHouse} />
+      </LoginHeader>
+      <LoginBody>
+        <h4 className="login__h4">반려동물 1등 플랫폼</h4>
+        <p className="login__p">모든 반려동물의 행복을 바라며</p>
+        <h1 className="login__h1 extraBold">포옹</h1>
+        <div className="img__section">
+          <img
+            src="/asset/login/Login_Img.svg"
+            alt="로그인 메인이미지"
+            className="login__img"
+          />
+        </div>
 
-          <div className="line__section">
-            <hr className="line" />
-            <span className="sns-login-text">SNS 로그인</span>
-            <hr className="line" />
-          </div>
+        <div className="line__section">
+          <hr className="line" />
+          <span className="sns-login-text">SNS 로그인</span>
+          <hr className="line" />
+        </div>
 
-          <div className="loginBtn__section">
-            <GoogleLoginButton />
-          </div>
-        </LoginBody>
-      </MainContainer>
+        <div className="loginBtn__section">
+          <GoogleLoginButton />
+        </div>
+      </LoginBody>
     </>
   );
 };
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,3 @@
-import GlobalStyle, { MainContainer } from '../style/global/global';
 import { Content } from '../style/global/global';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
@@ -7,17 +6,14 @@ import { Header } from '../style/menu/menu';
 function Menu() {
   return (
     <>
-      <GlobalStyle />
-      <MainContainer>
-        <Header className="extraBold">
-          포옹
-          <FontAwesomeIcon className="mypageIcon" icon={faCircleUser} />
-        </Header>
-        <Content>
-          <section></section>
-          <section></section>
-        </Content>
-      </MainContainer>
+      <Header className="extraBold">
+        포옹
+        <FontAwesomeIcon className="mypageIcon" icon={faCircleUser} />
+      </Header>
+      <Content>
+        <section></section>
+        <section></section>
+      </Content>
     </>
   );
 }
