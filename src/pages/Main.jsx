0045a7d2c6de112415cd1d6df5c526c851c869ee
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-11-04 11:28:58 +0900

    fix: Fix conflict

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -88,7 +88,7 @@ const Main = () => {
         <MainSection className="regular">
           <MainBtnDiv>
             <div>
-              <button onClick={() => handleClick('/care-list')}>
+              <button onClick={() => handleClick('/dog')}>
                 <img
                   src={'/asset/main/doglist.svg'}
                   alt="임보동물 목록아이콘"
