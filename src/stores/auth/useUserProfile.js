1f143c6da71ab4185e9ca8075f051167ea3a37a2
heeeione <heewon410@naver.com>
2024-11-06 10:57:47 +0900

    refactor: Add zustand persist to useUserProfile

--- src/stores/auth/useUserProfile.js
+++ src/stores/auth/useUserProfile.js
@@ -1,12 +1,18 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useUserProfile = create((set) => ({
-  user: {
-    nickName: '',
-    profileImage: '',
-  },
-  setUser: (userData) => set({ user: userData }),
-  clearUser: () => set({ user: { nickName: '', profileImage: '' } }),
-}));
+const useUserProfile = create(
+  persist(
+    (set) => ({
+      user: {
+        nickName: '',
+        profileImage: '',
+      },
+      setUser: (userData) => set({ user: userData }),
+      clearUser: () => set({ user: { nickName: '', profileImage: '' } }),
+    }),
+    { name: 'useUserProfile', partialize: (state) => ({ user: state.user }) },
+  ),
+);
 
 export default useUserProfile;
