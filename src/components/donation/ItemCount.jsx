9fa9ec7889b6b4c4dae84db4031b8d3d0cef37bc
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2024-11-07 19:54:56 +0900

    feat: 결제 시작 페이지 정보 동기화

--- src/components/donation/ItemCount.jsx
+++ src/components/donation/ItemCount.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ToggleBtn } from '../../style/donation/itemDetail';
+import { useDonation } from '../../stores/donation/useDonation';
 
 const ItemCount = ({ cnt, setTotalPrice, price }) => {
+  const { ItemCnt, setItemCnt } = useDonation();
   const handleCnt = (e) => {
     cnt.current += e.target.id === 'btn__plus' ? 1 : cnt.current > 0 ? -1 : 0;
+    setItemCnt(cnt.current * 1);
     setTotalPrice(cnt.current * price);
   };
   return (
