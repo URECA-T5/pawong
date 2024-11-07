3441db4938a154b98f5a0017f39365365242c91f
gwagjiug <kwjo0228@naver.com>
2024-11-07 11:04:08 +0900

    delete : api 콘솔로그 삭제

--- src/api/pet/care/list/list.js
+++ src/api/pet/care/list/list.js
@@ -5,7 +5,7 @@ const LOAD_CARE_LIST = '/pet/getAll';
 export const getCareInfo = async () => {
   try {
     const response = await request.get(LOAD_CARE_LIST);
-    console.log(response.data);
+
     return response.data;
   } catch (e) {
     console.error(`임보 목록 조회 오류 : ${e.response?.data?.message}`);
--- src/api/pet/care/userList/userList.js
+++ src/api/pet/care/userList/userList.js
@@ -5,7 +5,7 @@ const LOAD_USER_CARE_LIST = '/pet/getCareList';
 export const getUserCareList = async () => {
   try {
     const response = await request.get(LOAD_USER_CARE_LIST);
-    console.log(response.data);
+
     return response.data;
   } catch (e) {
     console.error(`유저 임보 목록 조회 오류 : ${e.response?.data?.message}`);
